f930073a26df53d9a1d67d2631c543bd5d766ea2
JeffKoMS <jeffko@microsoft.com>
2023-08-07 14:35:26 -0700

    Adding Fruit

---
+++ Allfiles/Labs/01/FruitAPI/Fruit.cs
@@ -0,0 +1,7 @@
+public class Fruit
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool Instock { get; set; }
+    
+}
---
+++ Allfiles/Labs/01/FruitAPI/FruitDb.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+class FruitDb : DbContext
+{
+    public FruitDb(DbContextOptions<FruitDb> options)
+        : base(options) { }
+
+    public DbSet<Fruit> Fruits => Set<Fruit>();
+}
