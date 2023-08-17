94d7e8911dbd4fc951dbfc10e67bbc6a21e15b7b
JeffKoMS <jeffko@microsoft.com>
2023-08-16 17:42:42 -0700

    Added FruitWebApp

---
+++ Allfiles/Labs/01/FruitWebApp/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.Extensions.Options;
+using Microsoft.Net.Http.Headers;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddHttpClient("FruitAPI", httpClient =>
+{
+    httpClient.BaseAddress = new Uri("https://fruitapi-testing.azurewebsites.net/fruitlist/");
+});
+
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
