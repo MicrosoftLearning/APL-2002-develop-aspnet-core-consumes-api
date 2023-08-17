c073bb7ddccb80644f2a37454c4bdae27ab9bdb4
JeffKoMS <jeffko@microsoft.com>
2023-08-16 18:03:05 -0700

    Added start

---
+++ Allfiles/Labs/01/FruitWebApp-start/Models/FruitModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FruitWebApp.Models;
+
+public class FruitModel
+{
+    [Key]
+    public int id { get; set; }
+
+    [Display(Name="Fruit Name")]
+    public string? name { get; set; }
+    [Display(Name ="Available?")]
+    public bool instock { get; set; }
+}
