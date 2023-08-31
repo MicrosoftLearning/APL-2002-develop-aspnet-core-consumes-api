8a7f37a62b62c11c75fa6bf59dfbbbd2dd621a80
JeffKoMS <jeffko@microsoft.com>
2023-08-31 10:26:08 -0700

    Created render project

---
+++ Allfiles/Labs/01/FruitWebApp-render/Pages/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using FruitWebApp.Models;
+using System.Text.Json;
+using Microsoft.Net.Http.Headers;
+using Microsoft.AspNetCore.Components;
+
+namespace FruitWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public IndexModel(IHttpClientFactory httpClientFactory, ILogger<IndexModel> logger)
+        {
+            _httpClientFactory = httpClientFactory;
+            _logger = logger;
+        }
+        [Inject]
+        public IEnumerable<FruitModel> FruitModels { get; set; }
+
+        public async Task OnGet()
+        {
+            using var httpRequestMessage = new HttpRequestMessage(
+                HttpMethod.Get,
+                "https://fruitapi-testing.azurewebsites.net/fruitlist");
+ 
+
+            var httpClient = _httpClientFactory.CreateClient();
+            var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                using var contentStream = await httpResponseMessage.Content.ReadAsStreamAsync();
+                FruitModels = await JsonSerializer.DeserializeAsync<IEnumerable<FruitModel>>(contentStream);
+            }
+        }
+
+ 
+    }
+   
+}
+
