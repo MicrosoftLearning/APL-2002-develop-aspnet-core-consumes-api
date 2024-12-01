2387ba6821e0fd835ef2b65a812c08730ecfa9bf
JeffKoMS <jeffko@microsoft.com>
2024-12-01 09:34:55 -0800

    Removed code from code-behind files

--- Allfiles/Labs/FruitWebApp-codebehind/Components/Pages/Add.razor.cs
+++ Allfiles/Labs/FruitWebApp-codebehind/Components/Pages/Add.razor.cs
@@ -24,27 +24,7 @@ public partial class Add : ComponentBase
 
     // Begin POST operation code
     private async Task Submit()
-    {
-        // Serialize the information to be added to the database
-        var jsonContent = new StringContent(JsonSerializer.Serialize(_fruitList),
-            Encoding.UTF8,
-            "application/json");
 
-        // Create the HTTP client using the FruitAPI named factory
-        var httpClient = HttpClientFactory.CreateClient("FruitAPI");
 
-        // Execute the POST request and store the response. The response will contain the new record's ID
-        using HttpResponseMessage response = await httpClient.PostAsync("/fruits", jsonContent);
-
-        // Check if the operation was successful, and navigate to the home page if it was
-        if (response.IsSuccessStatusCode)
-        {
-            NavigationManager?.NavigateTo("/");
-        }
-        else
-        {
-            Console.WriteLine("Failed to add fruit. Status code: {response.StatusCode}");
-        }
-    }
     // End POST operation code
 }
